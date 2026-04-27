77cc3bd0ae343fb385bec41fb869729fa953338d
kb478 <c23789389@mytudublin.ie>
2026-04-27 16:00:31 +0100

    Player movement

---
+++ Maynooth Ghost Girl/Assets/! Scripts/Player Scripts/Player_Camera.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+public class Player_Camera : MonoBehaviour
+{
+    
+    [Header("Camera Settings")]
+    public float sensitivity = 5F;
+    private float x_Rotation = 0f;
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    
+    private void Update()
+    {
+        // Get directions
+        float mouse_X = Input.GetAxis("Mouse X") * sensitivity;
+        float mouse_Y = Input.GetAxis("Mouse Y") * sensitivity;
+        
+        // Limit rotation to stop flipping
+        x_Rotation -= mouse_Y;
+        x_Rotation = Mathf.Clamp(x_Rotation, -90f, 90f);
+        
+        // Calculate and apply directions
+        transform.localRotation = Quaternion.Euler(x_Rotation, 0f, 0f);
+        transform.parent.Rotate(Vector3.up * mouse_X);
+    }
+}
---
+++ Maynooth Ghost Girl/Assets/! Scripts/Player Scripts/Player_Movement.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    [Header("Object Refs")]
+    [SerializeField] Camera player_Camera;
+    [SerializeField] private CharacterController controller;
+    
+    
+    [Header("Settigns")] 
+    [SerializeField] private float move_Speed = 5f;
+    [SerializeField] private float jump_Force = 5f;
+    [SerializeField] private float gravity = 10f;
+    
+    
+    private Vector3 velocity = Vector3.zero;
+    
+    
+    private void Update()
+    {
+        // Calculate directions
+        Vector3 movement = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        controller.Move(movement * move_Speed * Time.deltaTime);
+
+        // Check if grounded
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // If space pressed, jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity.y = Mathf.Sqrt(jump_Force * -2f * gravity);
+        }
+        
+        velocity.y += gravity * Time.deltaTime;
+        
+        // Apply movement
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
