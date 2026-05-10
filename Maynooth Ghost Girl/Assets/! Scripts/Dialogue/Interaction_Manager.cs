fd17754a8b53abf5ff17071e9caeaeae84e059d2
kb478 <c23789389@mytudublin.ie>
2026-05-10 16:27:18 +0100

    Animations hooked up

--- Maynooth Ghost Girl/Assets/! Scripts/Dialogue/Interaction_Manager.cs
+++ Maynooth Ghost Girl/Assets/! Scripts/Dialogue/Interaction_Manager.cs
@@ -34,8 +34,9 @@ public class Interaction_Manager : MonoBehaviour
     [SerializeField] private float face_Anim_Timer;
     
     
-    [Header("Ghost Girl")] 
+    [Header("- Ghost Girl -")] 
     [SerializeField] private GG_Dialogue_Lines gg_Dialogue_Lines;
+    [SerializeField] private Animator gg_Animator;
     public bool gg_First_Interaction = true;
     private bool gg_Final_Interaction = false;
     
@@ -47,20 +48,23 @@ public class Interaction_Manager : MonoBehaviour
     [SerializeField] private Texture[] gg_Talking_Textures;
     
     
-    [Header("Student 1")]
+    [Header("- Student 1 -")]
     [SerializeField] private St1_Dialogue_Lines st1_Dialogue_Lines;
+    [SerializeField] private Animator st1_Animator;
     public bool st1_Pleased = false;
     public bool st1_First_Interaction = true;
     
     
-    [Header("Student 2")]
+    [Header("- Student 2 -")]
     [SerializeField] private St2_Dialogue_Lines st2_Dialogue_Lines;
+    [SerializeField] private Animator st2_Animator;
     public bool st2_Pleased = false;
     public bool st2_First_Interaction = true;
     
     
-    [Header("Student 3")]
+    [Header("- Student 3 -")]
     [SerializeField] private St3_Dialogue_Lines st3_Dialogue_Lines;
+    [SerializeField] private Animator st3_Animator;
     public bool st3_Pleased = false;
     public bool st3_First_Interaction = true;
     
@@ -86,11 +90,13 @@ public class Interaction_Manager : MonoBehaviour
 
     #region --- NPC States ---
     
-    // State machines for each character to determine what dialogue to get next
+    // State machines for each character to determine what dialogue to get next and trigger animations
     
     public string Ghost_Girl_State()
     {
+        gg_Animator.SetBool("isTalking", true);
         StartCoroutine(GG_Facial_Interaction());
+        
         
         if (gg_First_Interaction == true)
         {
@@ -107,6 +113,7 @@ public class Interaction_Manager : MonoBehaviour
         
         if (st1_Pleased && st2_Pleased && st3_Pleased)
         {
+            gg_Animator.SetTrigger("Yippee_Trigger");
             gg_Final_Interaction = true;
             return "gg_End_Dialogue";
         }
@@ -124,6 +131,8 @@ public class Interaction_Manager : MonoBehaviour
 
     public string St1_State()
     {
+        st1_Animator.SetBool("isTalking", true);
+        
         // First time meeting
         if (st1_First_Interaction == true)
         {
@@ -142,6 +151,7 @@ public class Interaction_Manager : MonoBehaviour
             
             if (held_Item.tag == "St1 Object")
             {
+                st1_Animator.SetTrigger("Yippee_Trigger");
                 st1_Pleased = true;
                 Item_Given();
                 return "st1_Correct_Item";
@@ -168,6 +178,8 @@ public class Interaction_Manager : MonoBehaviour
     
     public string St2_State()
     {
+        st2_Animator.SetBool("isTalking", true);
+        
         if (st2_First_Interaction == true)
         {
             st2_First_Interaction = false;
@@ -185,6 +197,7 @@ public class Interaction_Manager : MonoBehaviour
             
             if (held_Item.tag == "St2 Object")
             {
+                st2_Animator.SetTrigger("Yippee_Trigger");
                 st2_Pleased = true;
                 Item_Given();
                 return "st2_Correct_Item";
@@ -211,6 +224,8 @@ public class Interaction_Manager : MonoBehaviour
     
     public string St3_State()
     {
+        st3_Animator.SetBool("isTalking", true);
+        
         if (st3_First_Interaction == true)
         {
             st3_First_Interaction = false;
@@ -228,6 +243,7 @@ public class Interaction_Manager : MonoBehaviour
             
             if (held_Item.tag == "St3 Object")
             {
+                st3_Animator.SetTrigger("Yippee_Trigger");
                 st3_Pleased = true;
                 Item_Given();
                 return "st3_Correct_Item";
@@ -290,6 +306,8 @@ public class Interaction_Manager : MonoBehaviour
    
     public void Post_Dialogue_Event()
     {
+        Stop_Animations();
+        
         if (gg_Final_Interaction == true)
             fade_Animation.Fade_Out();
 
@@ -298,6 +316,14 @@ public class Interaction_Manager : MonoBehaviour
             stew_Cheese.SetActive(true);
             gg_Stew_Cheese_Interaction = false;
         }
+    }
+
+    private void Stop_Animations()
+    {
+        gg_Animator.SetBool("isTalking", false);
+        st1_Animator.SetBool("isTalking", false);
+        st2_Animator.SetBool("isTalking", false);
+        st3_Animator.SetBool("isTalking", false);
     }
     
     #endregion
