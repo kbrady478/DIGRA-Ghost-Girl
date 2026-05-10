4b83021ca2ba54257c8f4674091e07f14ac29dd9
kb478 <c23789389@mytudublin.ie>
2026-05-10 15:10:27 +0100

    Crosshair Code

--- Maynooth Ghost Girl/Assets/! Scripts/Player Scripts/Interactor.cs
+++ Maynooth Ghost Girl/Assets/! Scripts/Player Scripts/Interactor.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.UIElements;
+
 
 interface IInteractable
 {
@@ -9,19 +11,78 @@ interface IInteractable
 
 public class Interactor : MonoBehaviour
 {
-    public Transform interactor_Source;
-    public float interact_Range;
-
+    [Header("References")]
+    [SerializeField] private LayerMask interactable_Layers;
+    [SerializeField] private Transform interactor_Source;
+    
+    [Header("Settings")]
+    [SerializeField] private float interact_Range;
+    
+    [Header("Crosshair")]
+    [SerializeField] private GameObject default_Crosshair;
+    [SerializeField] private GameObject npc_Crosshair;
+    [SerializeField] private GameObject item_Crosshair;
+  
+    
+    
     private void Update()
-    {
-        if (Input.GetKeyDown((KeyCode.E)))
-        {
-            Ray ray = new Ray(interactor_Source.position, interactor_Source.forward);
-            if (Physics.Raycast(ray, out RaycastHit hit, interact_Range))
-            {
-                if (hit.collider.gameObject.TryGetComponent(out IInteractable interact_Obj))
-                    interact_Obj.Interact();
-            }
-        }
-    }
-}
+    { 
+       Ray ray = new Ray(interactor_Source.position, interactor_Source.forward);
+       if (Physics.Raycast(ray, out RaycastHit hit, interact_Range, interactable_Layers))
+       {
+           
+          if (hit.collider.gameObject.TryGetComponent(out IInteractable interact_Obj))
+          {
+              // Check NPC
+              if (hit.collider.gameObject.layer == 7)
+              {
+                  if (npc_Crosshair.activeInHierarchy == false)
+                  {
+                      npc_Crosshair.SetActive(true);
+                      default_Crosshair.SetActive(false);
+                      item_Crosshair.SetActive(false);
+                  }
+
+                  if (Input.GetKeyDown(KeyCode.E))
+                  {
+                     interact_Obj.Interact();
+                     print("NPC interact attempt");
+                  }
+               
+              }
+
+              // Check Item
+              else if (hit.collider.gameObject.layer == 6)
+              {
+
+                  if (item_Crosshair.activeInHierarchy == false)
+                  {
+                      item_Crosshair.SetActive(true);
+                      npc_Crosshair.SetActive(false);
+                      default_Crosshair.SetActive(false);
+                  }
+                  
+                  if (Input.GetKeyDown(KeyCode.E))
+                  {
+                      interact_Obj.Interact();
+                      print("NPC interact attempt");
+                  }
+              }
+              
+          }// end Hit
+           
+       }// end Raycast 
+
+       else
+       {
+           if (default_Crosshair.activeInHierarchy == false)
+           {
+               npc_Crosshair.SetActive(false);
+               default_Crosshair.SetActive(true);
+               item_Crosshair.SetActive(false);
+           }
+       }// end default
+       
+    }// end Update()
+    
+}// end script
